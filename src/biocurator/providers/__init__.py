f12d6a76c879848181a6e38055a180febe77d1a8
dagsdags212 <jegsamson.dev@gmail.com>
2026-05-18 22:25:19 +0800

    chore: update providers exports for QueryBuilder refactor

--- src/biocurator/providers/__init__.py
+++ src/biocurator/providers/__init__.py
@@ -1,8 +1,29 @@
-from biocurator.providers.base import DatabaseConfig, DatabaseSearcher, NCBIDatabase, SearchCriteria, SequenceRecord
+from biocurator.providers.base import (
+    DatabaseConfig,
+    DatabaseSearcher,
+    NCBIDatabase,
+    QueryBuilder,
+    SearchCriteria,
+    SequenceRecord,
+)
 from biocurator.providers.ncbi_criteria import NCBISearchCriteria
+from biocurator.providers.ncbi_query_builders import get_builder
 from biocurator.providers.registry import ProviderRegistry
+from biocurator.providers.uniprot import UniProtQueryBuilder, UniProtSearchCriteria
 
 import biocurator.providers.ncbi      # registers "ncbi"
 import biocurator.providers.uniprot   # registers "uniprot"
 
-__all__ = ["DatabaseConfig", "DatabaseSearcher", "NCBIDatabase", "NCBISearchCriteria", "SearchCriteria", "SequenceRecord", "ProviderRegistry"]
+__all__ = [
+    "DatabaseConfig",
+    "DatabaseSearcher",
+    "NCBIDatabase",
+    "NCBISearchCriteria",
+    "QueryBuilder",
+    "SearchCriteria",
+    "SequenceRecord",
+    "ProviderRegistry",
+    "UniProtQueryBuilder",
+    "UniProtSearchCriteria",
+    "get_builder",
+]
