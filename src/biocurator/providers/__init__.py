be70376338b86c1fc2e0e591ab7d459496e900a0
dagsdags212 <jegsamson.dev@gmail.com>
2026-05-18 22:11:14 +0800

    fix: export NCBISearchCriteria from providers package

--- src/biocurator/providers/__init__.py
+++ src/biocurator/providers/__init__.py
@@ -1,7 +1,8 @@
-from biocurator.providers.base import DatabaseConfig, DatabaseSearcher, SearchCriteria, SequenceRecord
+from biocurator.providers.base import DatabaseConfig, DatabaseSearcher, NCBIDatabase, SearchCriteria, SequenceRecord
+from biocurator.providers.ncbi_criteria import NCBISearchCriteria
 from biocurator.providers.registry import ProviderRegistry
 
 import biocurator.providers.ncbi      # registers "ncbi"
 import biocurator.providers.uniprot   # registers "uniprot"
 
-__all__ = ["DatabaseConfig", "DatabaseSearcher", "SearchCriteria", "SequenceRecord", "ProviderRegistry"]
+__all__ = ["DatabaseConfig", "DatabaseSearcher", "NCBIDatabase", "NCBISearchCriteria", "SearchCriteria", "SequenceRecord", "ProviderRegistry"]
